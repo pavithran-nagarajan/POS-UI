5913555591de15b908a445c97924f02c06d47105
Pavithran Nagarajan <pavithran@appzgate.com>
2026-09-08 18:18:34 +0800

    Installed eslint

--- pos.admin.ui/src/app/app.routes.ts
+++ pos.admin.ui/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/guards/auth.guard';
 import { AuthLayout } from './layout/auth-layout/auth-layout';
 import { MainLayout } from './layout/main-layout/main-layout';
 
--- pos.admin.ui/src/app/app.ts
+++ pos.admin.ui/src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  changeDetection: ChangeDetectionStrategy.Eager,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<router-outlet></router-outlet>`,
 })
 export class App {}
